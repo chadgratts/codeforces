e2ca55f555fc274696385a73d165e50b94d8846d
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-07-05 22:33:34 -0400

    first solution

---
+++ watermelon.py
@@ -0,0 +1,16 @@
+# def watermelon(weight: int):
+#     if weight < 3:
+#         print("NO")
+#     elif weight > 3 and weight % 2 == 0:
+#         print("YES")
+#     else:
+#         print("NO")
+
+weight = int(input())
+
+if weight < 3:
+    print("NO")
+elif weight > 3 and weight % 2 == 0:
+    print("YES")
+else:
+    print("NO")
